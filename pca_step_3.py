be1dd9767db7bff7b8674c4014b14721bd90e953
nripesh <nripesh@baylabs.io>
2022-11-18 09:19:26 -0800

    format PCA better, no hardcode

--- pca_step_3.py
+++ pca_step_3.py
@@ -1,3 +1,4 @@
+from scipy.stats import pearsonr
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,7 +19,6 @@ def pca_and_plot_components():
     pca = PCA(n_components=10, whiten=WHITEN)
     pca.fit(X)
 
-    print(["{:0.1f}".format(var * 100) for var in pca.explained_variance_ratio_])
     print(
         "variance explained by 2 components: {:0.3f}".format(
             np.sum(pca.explained_variance_ratio_[:2])
@@ -29,6 +29,19 @@ def pca_and_plot_components():
             np.sum(pca.explained_variance_ratio_[:5])
         )
     )
+
+    # print correlations
+    r_comp_0_tlf = pearsonr(pca.components_[0], normalized_raw_data["y_tlf"])
+    print("comp 0, tlf corr:{:0.2f}".format(r_comp_0_tlf.statistic))
+
+    r_comp_0_hlf = pearsonr(pca.components_[0], normalized_raw_data["y_hlf"])
+    print("comp 0, hlf corr:{:0.2f}".format(r_comp_0_hlf.statistic))
+
+    r_comp_1_tlf = pearsonr(pca.components_[1], normalized_raw_data["y_tlf"])
+    print("comp 1, tlf corr:{:0.2f}".format(r_comp_1_tlf.statistic))
+
+    r_comp_1_hlf = pearsonr(pca.components_[1], normalized_raw_data["y_hlf"])
+    print("comp 1, hlf corr:{:0.2f}".format(r_comp_1_hlf.statistic))
 
     # draw components
     sns.set_theme()
@@ -42,13 +55,13 @@ def pca_and_plot_components():
         normalized_raw_data["x"],
         normalized_raw_data["y_hlf"],
         "r--",
-        label="HLF, r=0.98",
+        label="HLF, r={:0.2f}".format(r_comp_0_hlf.statistic),
     )
     axes[0].plot(
         normalized_raw_data["x"],
         normalized_raw_data["y_tlf"],
         "b-.",
-        label="TLF, r=-0.55",
+        label="TLF, r={:0.2f}".format(r_comp_0_tlf.statistic),
     )
     # axes[0].set_title('hlf component, principal component, 1')
     axes[0].legend()
@@ -59,16 +72,17 @@ def pca_and_plot_components():
         normalized_raw_data["x"],
         normalized_raw_data["y_tlf"],
         "b-.",
-        label="TLF, r=-0.065",
+        label="TLF, r={:0.2f}".format(r_comp_1_tlf.statistic),
     )
     axes[1].plot(
         normalized_raw_data["x"],
         normalized_raw_data["y_hlf"],
         "r--",
-        label="HLF, r=-0.32",
+        label="HLF, r={:0.2f}".format(r_comp_1_hlf.statistic),
     )
     axes[1].legend()
     plt.savefig("pca_comps_and_hlf_tlf.png")
+    plt.close()
 
 
 pca_and_plot_components()
